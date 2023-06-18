3b761a227ad6cfb4a4b2bce35e04b0a3c870ef22
Waleed <waleedakhtar2002@gmail.com>
2023-06-18 18:10:50 +0100

    Added social media linked to Sidebar/index.js Started development of Home Page

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -24,6 +25,24 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e' />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/waleed-akhtar-5b2397221/">
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4d"/>
+                </a>
+            </li>
+            <li>
+                <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href="https://github.com/Wawa-byte">
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4d"/>
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
